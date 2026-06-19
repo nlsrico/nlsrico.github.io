475b9a893d61e628531b5f22b66e4c88f0f0b762
nlsrico <najmieh.sa@gmail.com>
2026-06-19 21:42:59 +0330

    clean

--- _next/static/chunks/da4708ad64fedaed.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/0defbd4584ae369f.js",
-  "static/chunks/turbopack-fb3db173ef950cae.js"
-])
--- _next/static/chunks/e6e87893f36882ca.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/0defbd4584ae369f.js",
-  "static/chunks/turbopack-c53fde950cd2f890.js"
-])
